24d0d2583b62277dafe81831327198e1e763a741
G1 <koepm1999@gmail.com>
2024-06-23 23:57:30 +0900

    Feat: 1layer network

---
+++ example/simpleNet.py
@@ -0,0 +1,37 @@
+import numpy as np
+import setup_path
+from functions.softmax_function import softmax_function
+from functions.cross_entropy_error import CEE
+from gradient.gradient_descent import gradient_descent
+from gradient.numerical_diff import numerical_gradient
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self,x):
+        return np.dot(x,self.W)
+    
+    def loss(self,x,t):
+        z=self.predict(x)
+        y=softmax_function(z)
+        loss=CEE(y,t)
+
+        return loss
+    
+def main():
+    net=simpleNet()
+    print(f"{net.W=}")
+    x=np.array([0.6,0.9])
+    p=net.predict(x)
+    print(f"{p=} {np.argmax(p)=}")
+
+    t=np.array([0,0,1])
+    print(f"{net.loss(x,t)=}")
+
+    f=lambda w: net.loss(x,t)
+    dW=numerical_gradient(f,net.W)
+    print(dW)
+
+if __name__=="__main__":
+    main()
