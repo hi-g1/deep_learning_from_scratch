d94215d71deb4fe5206714f43dadae0977abc5b4
G1 <koepm1999@gmail.com>
2024-06-24 15:20:14 +0900

    Feat: add configuration loading from YAML and integrate with TwoLayerNet training

--- example/TwolayerNet_mnist.py
+++ example/TwolayerNet_mnist.py
@@ -1,17 +1,30 @@
 import numpy as np
+import os,sys
+sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
 from TwoLayerNet import TwoLayerNet
+import yaml
+
+with open('example\config.yaml', 'r') as f:
+    config=yaml.safe_load(f)
 
 (x_train, t_train), (x_test, t_test) = \
 load_mnist( normalize=False, one_hot_label=True)
 
 train_loss_list=[]
 
-iters_num=10000
+iters_num=config['iters_num']
+batch_size=config['batch_size']
+learning_rate = config['learning_rate']
+input_size = config['input_size']
+hidden_size = config['hidden_size']
+output_size = config['output_size']
 train_size=x_train.shape[0]
-batch_size=100
-learning_rate=0.1
-network=TwoLayerNet(input_size=784,hidden_size=50,output_size=10)
+
+network=TwoLayerNet(input_size,hidden_size,output_size)
+
+# 랜덤 시드 설정
+np.random.seed(0)
 
 for _ in range(iters_num):
     batch_mask=np.random.choice(train_size,batch_size)
